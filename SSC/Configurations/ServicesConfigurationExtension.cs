10121186a5d3d58bc9c1e7d2342832c6b925e8de
ksinoxx <forgotten.forfrik1a@gmail.com>
2025-05-24 21:46:36 +0500

    Update ServicesConfigurationExtension.cs

--- SSC/Configurations/ServicesConfigurationExtension.cs
+++ SSC/Configurations/ServicesConfigurationExtension.cs
@@ -1,14 +1,22 @@
-﻿
+
     public static class ServicesConfigurationExtension
     {
 
         public static void Configure(this WebApplicationBuilder builder)
         {
-        builder.Services.AddEndpointsApiExplorer();
-        builder.Services.AddSwaggerGen();
-        builder.Services.AddCors();
-        builder.Services.AddMvc();
-        builder.Services.AddControllers();
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+            builder.Services.AddCors();
+            builder.Services.AddMvc();
+            builder.Services.AddControllers();
+            builder.Services.AddDistributedMemoryCache();
+            builder.Services.AddSession(options =>
+            {
+                options.IdleTimeout = TimeSpan.FromMinutes(30);
+                options.Cookie.HttpOnly = true;
+                options.Cookie.IsEssential = true; 
+            });
+            builder.Services.AddControllersWithViews();
         }
 
     }
